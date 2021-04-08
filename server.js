312211af3c378e65ec55edeae08be743f8998034
Ravi kumar <ravikumar4799kuk@gmail.com>
2021-04-08 08:45:50 +0530

    add

--- server.js
+++ server.js
@@ -13,7 +13,7 @@ const cors = require('cors');
 
 // Cors
 const corsOptions = {
-  origin: process.env.ALLOWED_CLIENTS;
+  origin: process.env.ALLOWED_CLIENTS
   //['http://localhost:3000', 'http://localhost:5000', 'http://localhost:3300']
 }
 
