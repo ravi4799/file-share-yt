efe3a852da57e7d5db080f328a8ba3f9042242bd
Ravi kumar <ravikumar4799kuk@gmail.com>
2021-05-29 23:31:14 +0530

    add changes

--- server.js
+++ server.js
@@ -13,17 +13,13 @@ const cors = require('cors');
 
 // Cors
 const corsOptions = {
-
-    "origin": "*",
-    "methods": "GET,HEAD,PUT,PATCH,POST,DELETE",
-    "preflightContinue": false,
-    "optionsSuccessStatus": 204
-  
-  //['http://localhost:3000', 'http://localhost:5000', 'http://localhost:3300']
-}
-
+origin: process.env.ALLOWED_CLIENTS.split(',')
+//['http://localhost:3000', 'http://localhost:5000', 'http://localhost:3300']
+};
 
 app.use(cors(corsOptions));
+
+
 
 app.use(express.static('public'));
 app.use(express.json());
